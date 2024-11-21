d35eb19c2df59d041540a002ed28c9fa8266035b
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-11-21 22:42:08 +0800

    Import Data Kelamin

--- app/src/main/java/com/example/mvvm/Model/DataMahasiswa.kt
+++ app/src/main/java/com/example/mvvm/Model/DataMahasiswa.kt
@@ -1,10 +1,3 @@
 package com.example.mvvm.Model
 
-data class DataMahasiswa(
-    val nama: String ="",
-    val nim: String ="",
-    val email: String = "",
-    val gender: String = "",
-    val alamat: String = "",
-    val nohp: String = "",
-)
+data class DataMahasiswa()
