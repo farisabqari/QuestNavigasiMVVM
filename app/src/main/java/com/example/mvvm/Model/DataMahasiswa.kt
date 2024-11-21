4bec2aa8ffc421dfdcf20d2a77428c3ead92adfb
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-11-21 22:43:52 +0800

    Menambahkan Data Mahasiswa

--- app/src/main/java/com/example/mvvm/Model/DataMahasiswa.kt
+++ app/src/main/java/com/example/mvvm/Model/DataMahasiswa.kt
@@ -1,3 +1,10 @@
 package com.example.mvvm.Model
 
-data class DataMahasiswa()
+data class DataMahasiswa(
+    val nama: String ="",
+    val nim: String ="",
+    val email: String = "",
+    val gender: String = "",
+    val alamat: String = "",
+    val nohp: String = "",
+)
